cf9af7abcb6a9d52f8051898419897d15fca40c6
dfreire <dario.freire@gmail.com>
2016-06-30 23:16:13 +0100

    update

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-export default {};
+console.log('hello world');
+
+// var horizon = Horizon();
+// horizon.onReady(function() {
+//   document.querySelector('h1').innerHTML = 'App works!'
+// });
+// horizon.connect();
